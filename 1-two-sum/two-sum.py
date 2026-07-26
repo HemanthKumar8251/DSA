32412a1e47401105a293fedb97220be802051235
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-07-27 00:09:00 +0530

    Time: 3 ms (53.86%) | Memory: 20.8 MB (6.86%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map = {}
-        for i in range(len(nums)):
-            hash_map[nums[i]] = i
-        for i in range(len(nums)):
-            diff = target-nums[i]
-            if diff in hash_map and hash_map[diff]!=i:
-                return [i,hash_map[diff]]
-        return
+        # hash_map = {}
+        # for i in range(len(nums)):
+        #     hash_map[nums[i]] = i
+        # for i in range(len(nums)):
+        #     diff = target-nums[i]
+        #     if diff in hash_map and hash_map[diff]!=i:
+        #         return [i,hash_map[diff]]
+
+        # 2-Pointer Approach
+        enumerated_nums = [(num,i) for i,num in enumerate(nums)]
+        enumerated_nums.sort(key=lambda x: x[0])
+        i = 0
+        j = len(nums)-1
+        while i<j:
+            cur_sum = enumerated_nums[i][0]+enumerated_nums[j][0]
+            if cur_sum<target:
+                i+=1
+            elif cur_sum>target:
+                j-=1
+            else:
+                return [enumerated_nums[i][1],enumerated_nums[j][1]]
