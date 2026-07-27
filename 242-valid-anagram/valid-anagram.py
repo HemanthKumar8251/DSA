5358c40e7f52cd2333980ba86f1dfffe59053061
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-07-27 10:48:02 +0530

    Time: 13 ms (51.70%) | Memory: 19.3 MB (75.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,43 +8,43 @@ class Solution:
         # return s==t
 
         #Hashing
-        # if len(s)!=len(t):
-        #     return False
-        # hash_map = {}
-        # for i in s:
-        #     # if i in hash_map:
-        #     #     hash_map[i]+=1
-        #     # else:
-        #     #     hash_map[i]=1
-        #     hash_map[i] = hash_map.get(i,0)+1
+        if len(s)!=len(t):
+            return False
+        hash_map = {}
+        for i in s:
+            # if i in hash_map:
+            #     hash_map[i]+=1
+            # else:
+            #     hash_map[i]=1
+            hash_map[i] = hash_map.get(i,0)+1
                 
-        # for i in t:
-        #     if i not in hash_map or hash_map[i]==0:
-        #         return False
-        #     hash_map[i]-=1
+        for i in t:
+            if i not in hash_map or hash_map[i]==0:
+                return False
+            hash_map[i]-=1
         
-        # # for i in hash_map.values():
-        # #     if i!= 0:
-        # #         return False
+        # for i in hash_map.values():
+        #     if i!= 0:
+        #         return False
 
-        # return True
+        return True
 
         # Using Constant Space
         # Considering the ASCII values of the alphabets
         # Given - s and t consist of lowercase English letters
         # a=97,z=122,z-a=25
         # idx(alphabet) = ord(alphabet)-ord('a')
-        if len(s)!=len(t):
-            return False
-        letter_count = [0]*26
+        # if len(s)!=len(t):
+        #     return False
+        # letter_count = [0]*26
         
-        for i in s:
-            letter_count[ord(i)-ord('a')]+=1
+        # for i in s:
+        #     letter_count[ord(i)-ord('a')]+=1
         
-        for i in t:
-            j = ord(i)-ord('a')
-            if letter_count[j]==0:
-                return False
-            letter_count[j]-=1
+        # for i in t:
+        #     j = ord(i)-ord('a')
+        #     if letter_count[j]==0:
+        #         return False
+        #     letter_count[j]-=1
         
-        return True
+        # return True
