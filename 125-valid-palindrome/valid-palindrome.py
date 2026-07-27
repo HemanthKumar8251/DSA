1669c5eed326d062713aba6432f78766558cb068
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-07-27 12:31:35 +0530

    Time: 7 ms (81.04%) | Memory: 20.1 MB (28.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(i for i in list(s) if i not in " \"\'.?:;,<>`~/|@!#$%^&*()-+_=[]{\}")
+        # s = "".join(i for i in list(s) if i not in " \"\'.?:;,<>`~/|@!#$%^&*()-+_=[]{\}")
+        # Using the python method isalnum() to consider only alphabet and number
+        s = "".join(i for i in s if i.isalnum())
         s = s.lower()
         n = len(s)
         for i in range(n//2):
