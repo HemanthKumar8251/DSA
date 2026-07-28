455ffb4754c17d8f5905ad325b8747681118451a
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-07-28 12:17:42 +0530

    Time: 19 ms (19.66%) | Memory: 23.9 MB (16.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,6 +2,16 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hash_map = defaultdict(list)
-        for i in strs:
-            hash_map[''.join(sorted(i))].append(i)
+
+        # O(n*klogk) as we are using sorting
+        # for i in strs:
+        #     hash_map[''.join(sorted(i))].append(i)
+        # return list(hash_map.values())
+
+        # O(n*k) as we are using a count list
+        for s in strs:
+            count = [0]*26
+            for i in s:
+                count[ord(i)-ord('a')]+=1
+            hash_map[tuple(count)].append(s)
         return list(hash_map.values())
