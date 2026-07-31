ce7f770c9155f20aef15e7d0a534f18f1ad83d37
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-07-31 12:50:23 +0530

    Time: 4 ms (15.39%) | Memory: 19.3 MB (44.30%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -4,12 +4,13 @@ class Solution:
         INT_MAX = 2**31 - 1
         INT_MIN = -2**31
         #removing the leading whitespace
-        s = s.lstrip()
-        if not s:
-            return 0
-        #checking the sign
         i = 0
         n = len(s)
+        while i<n and s[i]==' ':
+            i+=1
+        if i==n:
+            return 0
+        #checking the sign
         sign = 1
         if s[i] == '+':
             i+=1
@@ -22,11 +23,11 @@ class Solution:
         #reading the integer skipping the leading zeros
         res = 0
         while i<n and s[i].isdigit():
-            digit = int(s[i])
-            res = res*10 + digit
-            if sign*res <= INT_MIN:
-                return INT_MIN
-            if sign*res >=INT_MAX:
-                return INT_MAX
+            res = res*10 + int(s[i]) 
             i+=1
-        return sign*res          
+        res = sign*res
+        if res <= INT_MIN:
+            return INT_MIN
+        if res >=INT_MAX:
+            return INT_MAX
+        return res          
