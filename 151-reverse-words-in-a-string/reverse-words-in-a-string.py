6bc7924b34f31269577905a5c9441235a31cf942
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-07-31 13:05:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.26%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,7 +1,13 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s = s.strip()
-        l = s.split(' ')
-        l = [x.strip() for x in l if x != '']
-        l = reversed(l)
-        return " ".join(l)
+        # Using extra space O(len(s))
+        # s = s.strip()
+        # l = s.split(' ')
+        # l = [x.strip() for x in l if x != '']
+        # l = reversed(l)
+        # return " ".join(l)
+
+        # Reduced Operations
+        return " ".join(s.split()[::-1])
+
+        # With space complexity O(1) is not possible in python as strings are immutable
