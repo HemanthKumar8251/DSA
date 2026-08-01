fa86112cb652119feafe3acc9f92f2da7be63f08
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-01 16:14:42 +0530

    Time: 8 ms (69.12%) | Memory: 19.1 MB (95.99%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        res = ['']*numRows
+        idx,d = 0,1
+        for char in s:
+            res[idx]+=char
+            if idx==0:
+                d = 1
+            elif idx==numRows-1:
+                d = -1
+            idx += d
+        return ''.join(res)
