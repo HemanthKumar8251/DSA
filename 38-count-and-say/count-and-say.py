ae09b7945c228928323f3ed501a32287640eb139
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-03 17:08:49 +0530

    Time: 8 ms (31.52%) | Memory: 19.1 MB (92.90%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,15 +1,31 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        res = '1'
-        for i in range(n-1):
-            count = 1
-            temp = ''
-            for j in range(1,len(res)):
-                if res[j]==res[j-1]:
-                    count += 1
-                else:
-                    temp += str(count)+res[j-1]
-                    count = 1
-            temp += str(count)+res[-1]
-            res = temp
+        # Iterative Approach
+        # res = '1'
+        # for i in range(n-1):
+        #     count = 1
+        #     temp = ''
+        #     for j in range(1,len(res)):
+        #         if res[j]==res[j-1]:
+        #             count += 1
+        #         else:
+        #             temp += str(count)+res[j-1]
+        #             count = 1
+        #     temp += str(count)+res[-1]
+        #     res = temp
+        # return res
+
+        #Recursive Approach
+        if n==1:
+            return '1'
+        prev = self.countAndSay(n-1)
+        res =''
+        count = 1
+        for j in range(1,len(prev)):
+            if prev[j]==prev[j-1]:
+                count += 1
+            else:
+                res += str(count)+prev[j-1]
+                count = 1
+        res += str(count)+prev[-1]
         return res
