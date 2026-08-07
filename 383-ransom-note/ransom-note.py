f08fa3397626fd79c9708282b238e3a5664c11b4
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-07 11:49:57 +0530

    Time: 15 ms (77.73%) | Memory: 19.9 MB (8.42%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,10 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hashMap = {}
-        for ch in magazine:
-            hashMap[ch]=hashMap.get(ch,0)+1
-        for ch in ransomNote:
-            if hashMap.get(ch,0)==0:
-                return False
-            hashMap[ch]-=1
-        return True
+        # hashMap = {}
+        # for ch in magazine:
+        #     hashMap[ch]=hashMap.get(ch,0)+1
+        # for ch in ransomNote:
+        #     if hashMap.get(ch,0)==0:
+        #         return False
+        #     hashMap[ch]-=1
+        # return True
+
+        # O(M+N) Time
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
