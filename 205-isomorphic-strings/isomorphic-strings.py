5a18172554d5e8a00c866d37a672264c24575472
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-07 12:22:46 +0530

    Time: 7 ms (36.51%) | Memory: 19.3 MB (46.96%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        sMap = {}
+        tMap = {}
+        if s==t:
+            return True
+        for i,j in zip(s,t):
+            if i in sMap and sMap[i]!=j:
+                return False
+            if j in tMap and tMap[j]!=i:
+                return False
+            sMap[i] = j
+            tMap[j] = i
+        return True
