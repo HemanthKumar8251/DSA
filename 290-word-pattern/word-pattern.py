840c337c3b5bf3e47baa6c7bbafbe8a7c078114c
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-07 12:49:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (69.32%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,15 +1,15 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        # patternMap = {}
-        # sMap = {}
-        # for i,j in zip(pattern,t):
-        #     if (i in sMap and sMap[i]!=j) or (j in tMap and tMap[j]!=i):
-        #         return False
-        #     sMap[i] = j
-        #     tMap[j] = i
-        # return True
-
-        # Usig sets and checking for exact pairs
         if len(pattern)!=len(s.split()):
             return False
-        return len(set(pattern)) == len(set(s.split())) == len(set(zip(pattern,s.split())))
+        patternMap = {}
+        sMap = {}
+        for i,j in zip(pattern,s.split()):
+            if (i in patternMap and patternMap[i]!=j) or (j in sMap and sMap[j]!=i):
+                return False
+            patternMap[i] = j
+            sMap[j] = i
+        return True
+
+        # Usig sets and checking for exact pairs
+        # return len(set(pattern)) == len(set(s.split())) == len(set(zip(pattern,s.split())))
