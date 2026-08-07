82a9df4d550845ac9d27c0060da60bf80da92e9d
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-07 12:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (22.00%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,10 +1,11 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        sMap = {}
-        tMap = {}
-        for i,j in zip(s,t):
-            if (i in sMap and sMap[i]!=j) or (j in tMap and tMap[j]!=i):
-                return False
-            sMap[i] = j
-            tMap[j] = i
-        return True
+        # sMap = {}
+        # tMap = {}
+        # for i,j in zip(s,t):
+        #     if (i in sMap and sMap[i]!=j) or (j in tMap and tMap[j]!=i):
+        #         return False
+        #     sMap[i] = j
+        #     tMap[j] = i
+        # return True
+        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
