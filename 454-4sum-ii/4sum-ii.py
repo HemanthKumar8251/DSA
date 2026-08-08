3227d2f8d9b2c8a9ef826494e218c21964f53e36
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-08 13:42:18 +0530

    Time: 437 ms (23.50%) | Memory: 19.4 MB (81.15%) - LeetSync

--- 454-4sum-ii/4sum-ii.py
+++ 454-4sum-ii/4sum-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        # count = 0
+        count = 0
 
         # O(n**4) Time, O(1) Space
         # for i in nums1:
@@ -22,16 +22,19 @@ class Solution:
         #                 count += hashMap[-(i+j+k)]
 
         # O(n**2) Time, O(n**2) Space
-        # hashMap = {}
-        # for i in nums3:
-        #     for j in nums4:
-        #         hashMap[i+j] = hashMap.get(i+j,0)+1
-        # for i in nums1:
-        #     for j in nums2:
-        #         if -(i+j) in hashMap:
-        #             count += hashMap[-(i+j)]
+        hashMap = {}
+        for i in nums3:
+            for j in nums4:
+                hashMap[i+j] = hashMap.get(i+j,0)+1
+        for i in nums1:
+            for j in nums2:
+                if -(i+j) in hashMap:
+                    count += hashMap[-(i+j)]
 
-        # return count
-        AB = collections.Counter(a+b for a in nums1 for b in nums2)
-        return sum(AB[-c-d] for c in nums3 for d in nums4)
+        return count
+
+        # Using built in counter for effectient building of hashMap and comprehensions
+        # to reduce it into 1 line of code
+        # AB = collections.Counter(a+b for a in nums1 for b in nums2)
+        # return sum(AB[-c-d] for c in nums3 for d in nums4)
         
