180c4f92925d1ae13343e4c7cdc0fddce9378081
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-08 14:22:10 +0530

    Time: 27 ms (85.28%) | Memory: 22.7 MB (90.81%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,29 +1,63 @@
 class MyHashMap:
 
+    # Using only nested lists and list operations
+    # def __init__(self):
+    #     self.hashmap = []
+
+    # def put(self, key: int, value: int) -> None:
+    #     for i in range(len(self.hashmap)):
+    #         if self.hashmap[i][0]==key:
+    #             self.hashmap[i][1]=value
+    #             break
+    #     else:
+    #         self.hashmap.append([key,value])
+        
+    # def get(self, key: int) -> int:
+    #     for i in range(len(self.hashmap)):
+    #         if self.hashmap[i][0]==key:
+    #             return self.hashmap[i][1]
+    #     return -1
+
+    # def remove(self, key: int) -> None:
+    #     for i in range(len(self.hashmap)):
+    #         if self.hashmap[i][0]==key:
+    #             self.hashmap.pop(i)
+    #             break
+
+    # Using extra _hash method for hashing the values into buckets to reduce time
     def __init__(self):
-        self.hashmap = []
+        self.size = 769
+        self.buckets = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        return key % self.size
 
     def put(self, key: int, value: int) -> None:
-        for i in range(len(self.hashmap)):
-            if self.hashmap[i][0]==key:
-                self.hashmap[i][1]=value
-                break
-        else:
-            self.hashmap.append([key,value])
+        bucket = self.buckets[self._hash(key)]
 
-        
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket[i] = (key, value)  # Update existing key
+                return
+
+        bucket.append((key, value))      # Insert new key
+
     def get(self, key: int) -> int:
-        for i in range(len(self.hashmap)):
-            if self.hashmap[i][0]==key:
-                return self.hashmap[i][1]
+        bucket = self.buckets[self._hash(key)]
+
+        for k, v in bucket:
+            if k == key:
+                return v
+
         return -1
 
     def remove(self, key: int) -> None:
-        for i in range(len(self.hashmap)):
-            if self.hashmap[i][0]==key:
-                self.hashmap.pop(i)
-                break
+        bucket = self.buckets[self._hash(key)]
 
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket.pop(i)
+                return
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
