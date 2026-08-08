c0dd6f1c7a6ecc700ef34b75632d446214d242e4
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-08 13:34:31 +0530

    Time: 442 ms (21.02%) | Memory: 19.4 MB (48.66%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        count = 0
+
+        # O(n**4) Time, O(1) Space
+        # for i in nums1:
+        #     for j in nums2:
+        #         for k in nums3:
+        #             for l in nums4:
+        #                 if i+j+k+l == 0:
+        #                     count += 1
+
+
+        # O(n**3) Time, O(n) Space
+        # hashMap = {}
+        # for i in nums4:
+        #     hashMap[i] = hashMap.get(i,0)+1
+        # for i in nums1:
+        #     for j in nums2:
+        #         for k in nums3:
+        #             if -(i+j+k) in hashMap:
+        #                 count += hashMap[-(i+j+k)]
+
+        # O(n**2) Time, O(n**2) Space
+        hashMap = {}
+        for i in nums3:
+            for j in nums4:
+                hashMap[i+j] = hashMap.get(i+j,0)+1
+        for i in nums1:
+            for j in nums2:
+                if -(i+j) in hashMap:
+                    count += hashMap[-(i+j)]
+
+        return count
