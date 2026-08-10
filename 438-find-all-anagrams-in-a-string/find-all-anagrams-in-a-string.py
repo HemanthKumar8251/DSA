62f6c5269e1af6255d8e4dac1923e3b81ab7187a
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-10 11:46:44 +0530

    Time: 51 ms (44.63%) | Memory: 19.8 MB (42.36%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -16,26 +16,52 @@ class Solution:
         n = len(p)
         if n>m:
             return []
-        
-        # O(m) Time, O(n) Space
         p_hash = Counter(p)
         s_hash = Counter()
         result = []
-        for i in range(m):
-            # Adding the char to the hashMap
-            s_hash[s[i]]+=1
+
+        # # O(m) Time, O(n) Space
+        # for i in range(m):
+        #     # Adding the char to the hashMap
+        #     s_hash[s[i]]+=1
             
-            # Removing the left most value as sliding window moves forward
+        #     # Removing the left most value as sliding window moves forward
+        #     if i>=n:
+        #         left_char = s[i-n]
+        #         if s_hash[left_char]==1:
+        #             del s_hash[left_char]
+        #         else:
+        #             s_hash[left_char]-=1
+            
+        #     # Addin to the reuslt
+        #     if p_hash == s_hash:
+        #         result.append(i-n+1)
+        
+        # Reducing the time for Comparision replacing hash Map
+        # Comparision with match varaible comparision
+        matches = 0
+        required_matches = len(p_hash)
+        for i in range(m):
+            right_char = s[i]
+            s_hash[right_char] += 1
+
+            # Matches is only incremented if the frequency of that number is equal
+            if right_char in p:
+                if s_hash[right_char] == p_hash[right_char]:
+                    matches += 1
+                elif s_hash[right_char] == p_hash[right_char]+1:
+                    matches -= 1
+            
             if i>=n:
                 left_char = s[i-n]
-                if s_hash[left_char]==1:
-                    del s_hash[left_char]
-                else:
-                    s_hash[left_char]-=1
-            
-            # Addin to the reuslt
-            if p_hash == s_hash:
+                if left_char in p_hash and s_hash[left_char] == p_hash[left_char]:
+                    matches-=1
+                s_hash[left_char]-=1
+                if left_char in p_hash and s_hash[left_char] == p_hash[left_char]:
+                    matches+=1
+                    
+            if matches==required_matches:
                 result.append(i-n+1)
-        
+
         return result
                 
