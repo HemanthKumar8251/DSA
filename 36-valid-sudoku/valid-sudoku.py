4f56e6f3ecce1ae987dec24f48624d096658ec01
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-10 12:46:25 +0530

    Time: 3 ms (74.15%) | Memory: 19.2 MB (73.35%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,6 +1,9 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
+        # O(9*9) Time - Constant Time, O(3*9*9) - Constant Space
+        # We can reduce space to O(3*3) but time complexity increases as we need to run
+        # 9*9 loop 3 times 
         row_map = [[False]*9 for _ in range(9)]
         col_map = [[False]*9 for _ in range(9)]
         box_map = [[[False]*9 for _ in range(3)] for j in range(3)]
