0c536926e46c44768d168ad9535e251597d0e10a
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-10 11:03:01 +0530

    Time: 184 ms (26.08%) | Memory: 20 MB (15.78%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,12 +1,40 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        n = len(p)
+
+        # O(mxn) Time and O(m+n) Space
+        # n = len(p)
+        # m = len(s)
+        # hashP = Counter(p)
+        # result = []
+        # for i in range(m-n+1):
+        #     if s[i] not in p:
+        #         continue
+        #     if hashP==Counter(s[i:i+n]):
+        #         result.append(i)
+        # return result
         m = len(s)
-        hashP = Counter(p)
+        n = len(p)
+        if n>m:
+            return []
+        
+        p_hash = Counter(p)
+        s_hash = Counter()
         result = []
-        for i in range(m-n+1):
-            if s[i] not in p:
-                continue
-            if hashP==Counter(s[i:i+n]):
-                result.append(i)
+        for i in range(m):
+            # Adding the char to the hashMap
+            s_hash[s[i]]+=1
+            
+            # Removing the left most value as sliding window moves forward
+            if i>=n:
+                left_char = s[i-n]
+                if s_hash[left_char]==1:
+                    del s_hash[left_char]
+                else:
+                    s_hash[left_char]-=1
+            
+            # Addin to the reuslt
+            if p_hash == s_hash:
+                result.append(i-n+1)
+        
         return result
+                
