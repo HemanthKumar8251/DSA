10f4d1c7dc516869b29702a2fcfba68c275104e4
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-10 12:43:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (34.08%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        row_map = [[False]*9 for _ in range(9)]
+        col_map = [[False]*9 for _ in range(9)]
+        box_map = [[[False]*9 for _ in range(3)] for j in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != ".":
+                    ele = int(board[i][j])
+                    if row_map[i][ele-1] or col_map[j][ele-1] or box_map[i//3][j//3][ele-1]:
+                        return False
+                    row_map[i][ele-1] = True
+                    col_map[j][ele-1] = True
+                    box_map[i//3][j//3][ele-1] = True
+        return True
+            
