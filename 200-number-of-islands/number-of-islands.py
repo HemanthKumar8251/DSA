ac04bbccb4edaa4eee34f61e645d1c29a3a76f2d
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-11 11:12:20 +0530

    Time: 249 ms (44.17%) | Memory: 21.5 MB (91.43%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        def bfs(row,col):
+            directions = [(0,-1),(-1,0),(1,0),(0,1)]
+            q = deque([(row,col)])
+            while q:
+                row,col = q.popleft()
+                grid[row][col]='-1'
+                for dr,dc in directions:
+                    r,c = dr+row,dc+col
+                    if 0<=r<m and 0<=c<n and grid[r][c]=='1':
+                            q.append((r,c))
+                            grid[r][c]='-1'
+        islands = 0    
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=='1':
+                    islands+=1
+                    bfs(i,j)
+        
+        return islands
