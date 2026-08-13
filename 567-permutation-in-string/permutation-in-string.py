c85c6b8cf845de7d99fd47e5a1d53b4b175d60c4
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-13 15:04:27 +0530

    Time: 11 ms (96.35%) | Memory: 19.4 MB (18.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,13 +1,38 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        freq_s1 = Counter(s1)
-        freq_window = Counter()
-        left = 0
-        for right in range(len(s2)):
-            freq_window[s2[right]] = freq_window.get(s2[right],0)+1
-            if right-left+1>len(s1):
-                freq_window[s2[left]]-=1
-                left +=1
-            if right-left+1==len(s1) and freq_s1 & freq_window == freq_s1:
+        ##Using Counter which gives a freq dict
+        # freq_s1 = Counter(s1)
+        # freq_window = Counter()
+        # left = 0
+        # for right in range(len(s2)):
+        #     freq_window[s2[right]] = freq_window.get(s2[right],0)+1
+        #     if right-left+1>len(s1):
+        #         freq_window[s2[left]]-=1
+        #         left +=1
+        #     if right-left+1==len(s1) and freq_s1 & freq_window == freq_s1:
+        #         return True
+        # return False
+
+        # Using a list of size 26 to hash characters with ord(char)-ord('a')
+        if len(s1)>len(s2):
+            return False
+        
+        s1_freq = [0]*26
+        window_freq = [0]*26
+
+        for char in s1:
+            s1_freq[ord(char)-ord('a')]+=1
+
+        for i in range(len(s1)):
+            window_freq[ord(s2[i])-ord('a')]+=1
+
+        if s1_freq == window_freq:
+            return True
+
+        for i in range(len(s1),len(s2)):
+            window_freq[ord(s2[i])-ord('a')]+=1
+            window_freq[ord(s2[i-len(s1)])-ord('a')]-=1
+            if window_freq==s1_freq:
                 return True
+
         return False
