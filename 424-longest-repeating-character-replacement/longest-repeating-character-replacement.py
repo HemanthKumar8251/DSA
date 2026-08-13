548420dae1ddcdab73b07ddd9e5b8357e7b00da7
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-13 14:13:31 +0530

    Time: 103 ms (43.34%) | Memory: 19.7 MB (15.21%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,9 +5,8 @@ class Solution:
         res = 0
         for right in range(len(s)):
             freq[s[right]]=freq.get(s[right],0)+1
-            max_freq = max(freq.values())
-            window_size = right-left+1
-            if window_size - max_freq > k:
+            max_freq = max(freq.values()) 
+            if right-left+1 - max_freq > k:
                 freq[s[left]]-=1
                 left += 1
             res = max(right-left+1,res)
