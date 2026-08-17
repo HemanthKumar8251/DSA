b95f7e12f729ab2726d7ebbc3f5f38f50917ac1b
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-17 12:08:33 +0530

    Time: 771 ms (22.36%) | Memory: 27.2 MB (63.79%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,33 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # Merge Sort - O(nlogn) Time,O(n) Space
+        def mergeSort(nums,low,high):
+            if low==high:
+                return
+            mid = (low+high)//2
+            mergeSort(nums,low,mid)
+            mergeSort(nums,mid+1,high)
+            merge(nums,low,mid,high)
+        def merge(nums,low,mid,high):
+            left=low
+            right=mid+1
+            temp = []
+            while left<=mid and right<=high:
+                if nums[left]<nums[right]:
+                    temp.append(nums[left])
+                    left+=1
+                else:
+                    temp.append(nums[right])
+                    right+=1
+            while left<=mid:
+                    temp.append(nums[left])
+                    left+=1
+            while right<=high:
+                    temp.append(nums[right])
+                    right+=1
+            for i in range(low,high+1):
+                nums[i]=temp[i-low]
+
+        mergeSort(nums,0,len(nums)-1)
+        return nums
+        # Quick Sort - O()
