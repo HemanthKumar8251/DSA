cf48ca69d77ab84930d8c065b198d9d6863e9e4a
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-18 21:40:14 +0530

    Time: 7 ms (72.00%) | Memory: 22.4 MB (86.30%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,9 +1,21 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x:x[0])
+        # intervals.sort(key=lambda x:x[0])
+        # merged = []
+        # prev = intervals[0]
+        # for interval in intervals[1:]:
+        #     if interval[0]<=prev[1]:
+        #         prev[1] = max(interval[1],prev[1])
+        #     else:
+        #         merged.append(prev)
+        #         prev = interval
+        # merged.append(prev)
+        # return merged
+
+        intervals.sort(key=lambda x: x[0])
         merged = []
         prev = intervals[0]
-        for interval in intervals[1:]:
+        for interval in intervals:
             if interval[0]<=prev[1]:
                 prev[1] = max(interval[1],prev[1])
             else:
@@ -11,3 +23,13 @@ class Solution:
                 prev = interval
         merged.append(prev)
         return merged
+
+
+
+
+
+
+
+
+
+
