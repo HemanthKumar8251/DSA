ff64e31f5e26251b499c36696834e769f355558d
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-18 11:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (43.62%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,6 +1,19 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        # O(n) Time Complexity, List Traversal
         for i,num in enumerate(nums):
             if num>=target:
                 return i
+        return len(nums)
+
+        # O(log n) Time Complexity, Using Binary Search
+        i,j=0,len(nums)-1
+        while i<j:
+            mid = (i+j)//2
+            if nums[mid]<target:
+                i=mid+1
+            elif nums[mid]>target:
+                j=mid-1
+            else:
+                return mid
         return len(nums)
