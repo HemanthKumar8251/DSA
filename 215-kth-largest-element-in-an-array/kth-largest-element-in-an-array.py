8a336e0598f1392311e788f4cf827098d159f187
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-19 12:18:17 +0530

    Time: 70 ms (71.57%) | Memory: 31.1 MB (47.88%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,20 +1,60 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
 
-        ## Using max heap and poping
-        nums = [-x for x in nums]
-        heapq.heapify(nums)
-        print(nums)
-        for i in range(k-1):
-            heapq.heappop(nums)
-        return -1*heapq.heappop(nums)
+        ## Using max heap and poping until k ele
+        # nums = [-x for x in nums]
+        # heapq.heapify(nums)
+        # print(nums)
+        # for i in range(k-1):
+        #     heapq.heappop(nums)
+        # return -1*heapq.heappop(nums)
 
+        ## Using min heap and sorting only k largest elements
         # heap = nums[:k]
         # heapq.heapify(heap)
-        
         # for num in nums[k:]:
         #     if num > heap[0]:
         #         heapq.heappop(heap)
         #         heapq.heappush(heap, num)
-        
         # return heap[0]
+
+        # ## Using quick select 2way partition - Time Limit Exceeded 
+        # def quickSelect(nums,low,high):
+        #     if low<=high:
+        #         prt_idx = partition(nums,low,high)
+        #         if prt_idx==len(nums)-k:
+        #             return nums[prt_idx]
+        #         elif prt_idx<len(nums)-k:
+        #             return quickSelect(nums,prt_idx+1,high)
+        #         else:
+        #             return quickSelect(nums,low,prt_idx-1)
+        # def partition(nums,low,high):
+        #     pivot_idx = random.randint(low, high)
+        #     nums[low],nums[pivot_idx]=nums[pivot_idx],nums[low]
+        #     pivot = nums[low]
+        #     i,j = low,high
+        #     while i<j:
+        #         while i<=high-1 and nums[i]<=pivot:
+        #             i+=1
+        #         while j>=low+1 and nums[j]>pivot:
+        #             j-=1
+        #         if i<j:
+        #             nums[i],nums[j]=nums[j],nums[i]
+        #     nums[low],nums[j]=nums[j],nums[low]
+        #     return j
+        # return quickSelect(nums,0,len(nums)-1)
+
+        # Quick select 3-way partition and extra space
+        if not nums: return
+        pivot = random.choice(nums)
+        lt = [x for x  in nums if x<pivot]
+        eq = [x for x  in nums if x==pivot]
+        gt = [x for x  in nums if x>pivot]
+        L,M = len(gt), len(eq)
+
+        if k<=L:
+            return self.findKthLargest(gt,k)
+        elif k>L+M:
+            return self.findKthLargest(lt,k-L-M)
+        else:
+            return eq[0]
