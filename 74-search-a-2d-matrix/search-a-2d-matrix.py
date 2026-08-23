d7d4860ed4749da3c84a2ffbe1b360f868b4e6ba
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-23 16:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (39.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,34 +1,49 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # Using 2 Binary Searches one for Row and one for Col
-        m, n = len(matrix), len(matrix[0])
-        # 1. Binary search to find the correct row
-        mlow, mhigh = 0, m - 1
-        target_row = -1
+        # # Using 2 Binary Searches one for Row and one for Col
+        # m, n = len(matrix), len(matrix[0])
+        # # 1. Binary search to find the correct row
+        # mlow, mhigh = 0, m - 1
+        # target_row = -1
         
-        while mlow <= mhigh:
-            mid = (mlow + mhigh) // 2
-            # Check if target falls within the boundary of the current row
-            if matrix[mid][0] <= target <= matrix[mid][-1]:
-                target_row = mid
-                break
-            elif target < matrix[mid][0]:
-                mhigh = mid - 1
-            else:
-                mlow = mid + 1
+        # while mlow <= mhigh:
+        #     mid = (mlow + mhigh) // 2
+        #     # Check if target falls within the boundary of the current row
+        #     if matrix[mid][0] <= target <= matrix[mid][-1]:
+        #         target_row = mid
+        #         break
+        #     elif target < matrix[mid][0]:
+        #         mhigh = mid - 1
+        #     else:
+        #         mlow = mid + 1
                 
-        # If target_row is still -1, the target is out of the matrix overall bounds
-        if target_row == -1:
-            return False
+        # # If target_row is still -1, the target is out of the matrix overall bounds
+        # if target_row == -1:
+        #     return False
             
-        # 2. Binary search within the identified row
-        nlow, nhigh = 0, n - 1
-        while nlow <= nhigh:
-            mid = (nlow + nhigh) // 2
-            if matrix[target_row][mid] == target:
-                return True
-            elif target < matrix[target_row][mid]:
-                nhigh = mid - 1
+        # # 2. Binary search within the identified row
+        # nlow, nhigh = 0, n - 1
+        # while nlow <= nhigh:
+        #     mid = (nlow + nhigh) // 2
+        #     if matrix[target_row][mid] == target:
+        #         return True
+        #     elif target < matrix[target_row][mid]:
+        #         nhigh = mid - 1
+        #     else:
+        #         nlow = mid + 1 
+        # return False
+
+        # Considering the 2D Matrics as a flat array
+        m,n=len(matrix),len(matrix[0])
+        low,high=0,m*n-1
+        while low<=high:
+            mid = low+(high-low)//2
+            row = mid//n
+            col = mid%n
+            if matrix[row][col]>target:
+                high = mid-1
+            elif matrix[row][col]<target:
+                low = mid+1
             else:
-                nlow = mid + 1 
+                return True
         return False
