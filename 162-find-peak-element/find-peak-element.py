c229487c59714f30be33d986888a253bebd8ef3f
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-23 17:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.57%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -5,8 +5,9 @@ class Solution:
             mid = (low+high)//2
             if nums[mid]<nums[mid+1]:
                 low = mid+1
-            elif nums[mid]>nums[mid+1]:
-                high = mid
+            # elif nums[mid]>nums[mid+1]:
+            #     high = mid
             else:
-                return mid
+                # return mid
+                high = mid
         return low
