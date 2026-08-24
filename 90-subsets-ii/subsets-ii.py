70efc1f54a128380985819fd8d07c6e57b4247b9
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-24 13:18:33 +0530

    Time: 7 ms (5.79%) | Memory: 19.4 MB (60.38%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,11 +1,12 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         # Using Back Tracking (Recursion)
+        nums = sorted(nums)
         res = []
         def backtracking(cur_arr,i):
             if i==len(nums):
-                if sorted(cur_arr) not in res:
-                    res.append(list(sorted(cur_arr)))
+                if cur_arr not in res:
+                    res.append(list(cur_arr))
                 return
             cur_arr.append(nums[i])
             backtracking(cur_arr,i+1)
