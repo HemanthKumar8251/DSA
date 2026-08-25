83a8cdf20346215e53013c46eac5b37058521982
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-25 09:59:25 +0530

    Time: 1 ms (45.11%) | Memory: 19.4 MB (66.70%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,21 +1,40 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        result = []
-        cur_arr = []
-        used = defaultdict(bool)
-        def permutations():
-            if len(cur_arr)==n:
-                result.append(list(cur_arr))
-                return
-            for i in range(n):
-                if used[i]:
-                    continue
-                used[i]=True
-                cur_arr.append(nums[i])
-                permutations()
-                cur_arr.pop()
-                used[i]=False
+        # n = len(nums)
+        # result = []
+        # cur_arr = []
+        # used = defaultdict(bool)
+        # def permutations():
+        #     if len(cur_arr)==n:
+        #         result.append(list(cur_arr))
+        #         return
+        #     for i in range(n):
+        #         if used[i]:
+        #             continue
+        #         used[i]=True
+        #         cur_arr.append(nums[i])
+        #         permutations()
+        #         cur_arr.pop()
+        #         used[i]=False
         
-        permutations()
+        # permutations()
+        # return result
+
+        # Using a set for storing seen ele as all are unique 
+        # Even if there were duplicates it helps avoid duplicate permutations
+        result,perm = [],[]
+        seen = set()
+        self.permutation(perm,result,nums,seen)
         return result
+
+    def permutation(self,perm,result,nums,seen):
+        if len(nums)==len(perm):
+            result.append(perm.copy())
+            return
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+                perm.append(num)
+                self.permutation(perm,result,nums,seen)
+                seen.remove(num)
+                perm.pop()
