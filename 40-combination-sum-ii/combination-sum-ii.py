ac267b16e838e16b6f7b1332b424fc25ae3b8b5b
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-29 23:12:57 +0530

    Time: 4 ms (67.40%) | Memory: 19.4 MB (90.50%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -6,9 +6,7 @@ class Solution:
         return result
 
     def combinationsSum(self, candidates, target, combi, sum, result, idx):
-        if sum>target:
-            return
-        elif sum==target:
+        if target == 0:
             result.append(combi[:])
             return
         for i in range(idx,len(candidates)):
@@ -18,6 +16,6 @@ class Solution:
                 break
             combi.append(candidates[i])
             sum += candidates[i]
-            self.combinationsSum(candidates,target,combi,sum,result,i+1)
+            self.combinationsSum(candidates,target-candidates[i],combi,sum,result,i+1)
             combi.pop()
             sum -= candidates[i]
