f236511d114136b212e2e2ee7e67c23e71c81da2
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-08-30 13:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.48%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        numbers = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        letters = []
+        for digit in digits:
+            letters.append(numbers[digit])
+        combi_len = len(digits)
+        result = []
+
+        def combinationsLetters(combi_len,letters,idx,result,combi):
+            if len(combi)==combi_len:
+                result.append("".join(combi))
+                return 
+            for letter in letters[idx]:
+                combi.append(letter)
+                combinationsLetters(combi_len,letters,idx+1,result,combi)
+                combi.pop()
+        
+        combinationsLetters(combi_len,letters,0,result,[])
+        return result
