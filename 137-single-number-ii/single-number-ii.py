49a4d3e28e233a8bae68e9d9de2180015445a24e
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-09-01 12:05:17 +0530

    Time: 28 ms (23.68%) | Memory: 20.7 MB (46.69%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -5,8 +5,9 @@ class Solution:
             bit_sum = 0
             for num in nums:
                 bit_sum += num>>i&1
-            bit_sum %= 3
-            res |= bit_sum<<i
-        if res >= 2**31:
-            res -= 2**32
+            if bit_sum % 3:
+                if i == 31:
+                    res -= (1 << i)
+                else:
+                    res |= (1 << i)
         return res
