5c945c6df81c09caf741ee3152ea984570b2bf44
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-09-04 14:33:18 +0530

    Time: 3 ms (95.48%) | Memory: 20.3 MB (39.56%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0]*(n+1)
+        sub = 1
+        for i in range(1,n+1):
+            if i == 2*sub:
+                sub = i
+            dp[i]=dp[i-sub]+1
+        return dp
