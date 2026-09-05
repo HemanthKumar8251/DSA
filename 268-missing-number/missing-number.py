5d4f551860b11b1c250ee45d95af1e5fc0820122
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-09-05 19:18:36 +0530

    Time: 7 ms (36.64%) | Memory: 20.3 MB (95.56%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,5 +1,22 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        missingNum = (n*(n+1)//2)-sum(nums)
-        return missingNum
+        # # Using Math
+        # n = len(nums)
+        # missingNum = (n*(n+1)//2)-sum(nums)
+        # return missingNum
+
+        # # Logical way of using the index values
+        # # Sum of the index values is the total sum - each element
+        # # Gives the missing number 
+        # res = len(nums)
+        # for i,num in enumerate(nums):
+        #     res += i-num
+        # return res
+
+        # Using bit Manipulation with similar logic as above
+        # Using XOR we get a^b^b = a
+        # res ^ i ^ num
+        res = len(nums)
+        for i,num in enumerate(nums):
+            res = res^i^num
+        return res
