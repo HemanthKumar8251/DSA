c6b487c712bb599baacb43775743bfd60f6e7ec1
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-09-19 11:07:17 +0530

    Time: 79 ms (23.28%) | Memory: 24.2 MB (26.41%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -6,8 +6,7 @@ class StockSpanner:
     def next(self, price: int) -> int:
         span = 1
         while self.st and self.st[-1][0]<=price:
-            span+=self.st[-1][1]
-            self.st.pop()
+            span+=self.st.pop()[1]
         self.st.append([price,span])
         return span
 
