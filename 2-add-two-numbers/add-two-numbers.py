d5de85f7916f4abf8e23aa2534380d332127bee4
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2026-09-21 11:01:11 +0530

    Time: 1 ms (75.44%) | Memory: 19.3 MB (43.12%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,34 +5,52 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
+        # carry = 0
+        # dummy = ListNode()
+        # prev = dummy
+        # while l1 and l2:
+        #     node = ListNode(val=(l1.val+l2.val+carry)%10)
+        #     carry = (l1.val+l2.val+carry)//10
+        #     prev.next = node
+        #     prev = node
+        #     l1 = l1.next
+        #     l2 = l2.next
+        # while l1 and carry==1:
+        #     node = ListNode(val=(l1.val+carry)%10)
+        #     carry = (l1.val+carry)//10
+        #     prev.next = node
+        #     prev = node
+        #     l1 = l1.next
+        # while l2 and carry==1:
+        #     node = ListNode(val=(l2.val+carry)%10)
+        #     carry = (l2.val+carry)//10
+        #     prev.next = node
+        #     prev = node
+        #     l2 = l2.next
+        # if l1:
+        #     prev.next = l1
+        # if l2:
+        #     prev.next = l2
+        # if carry == 1:
+        #     prev.next = ListNode(val=1)
+        # return dummy.next
+
         carry = 0
         dummy = ListNode()
         prev = dummy
-        while l1 and l2:
-            node = ListNode(val=(l1.val+l2.val+carry)%10)
-            carry = (l1.val+l2.val+carry)//10
-            prev.next = node
-            prev = node
-            l1 = l1.next
-            l2 = l2.next
-        while l1 and carry==1:
-            node = ListNode(val=(l1.val+carry)%10)
-            carry = (l1.val+carry)//10
-            prev.next = node
-            prev = node
-            l1 = l1.next
-        while l2 and carry==1:
-            node = ListNode(val=(l2.val+carry)%10)
-            carry = (l2.val+carry)//10
-            prev.next = node
-            prev = node
-            l2 = l2.next
-        if l1:
-            prev.next = l1
-        if l2:
-            prev.next = l2
-        if carry == 1:
-            prev.next = ListNode(val=1)
-        return dummy.next
-
+        while l1 or l2 or carry:
+            total = carry
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
             
+            num = total%10
+            carry = total//10
+            node = ListNode(val=num)
+            prev.next = node
+            prev = node
+            
+        return dummy.next
